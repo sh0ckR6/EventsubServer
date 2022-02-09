ee675bc23fb45efe4fe9ee5cb715c5703723cbac
Kaleb <sh0ck.04@outlook.com>
2022-02-09 00:24:55 -0500

    Create basic express server
    Nothing fancy, just handles incoming EventSub requests (verification, notification, revocation)

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+
+const MESSAGE_TYPE = 'Twitch-Eventsub-Message-Type'.toLowerCase();
+
+const VERIFICATION_MESSAGE = 'webhook_callback_verification';
+const NOTIFICATION_MESSAGE = 'notification';
+const REVOCATION_MESSAGE = 'revocation';
+
+app.post('/eventsub', (req, res) => {
+    let notification = JSON.parse(req.body);
+
+    if (req.headers[MESSAGE_TYPE] === VERIFICATION_MESSAGE) {
+        res.status(200).send(notification.challenge);
+        return;
+    }
+
+    if (req.headers[MESSAGE_TYPE] === NOTIFICATION_MESSAGE) {
+        console.log('Incoming event!');
+        res.sendStatus(204);
+        return;
+    }
+
+    if (req.headers[MESSAGE_TYPE] === REVOCATION_MESSAGE) {
+        console.log(`EventSub subscription for ${notification.subscription.type} revoked! Reason: ${notification.subscription.status}`);
+        res.sendStatus(204);
+        return;
+    }
+});
